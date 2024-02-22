7f381a35674dee4effd45b7b1a398277a480d533
AntSch <antoine97410@hotmail.fr>
2024-02-22 15:00:49 +0100

    Add category page with the related modules and components
    Add icon for Search component

---
+++ src/app/category/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ProductGrid from "@/components/Shop/ProductGrid/ProductGrid";
+import Header from "@/components/Shop/Header/Header";
+import SideBar from "@/components/Shop/SideBar/SideBar";
+import Search from "@/components/Shop/Search/Search";
+import styles from "./category.module.css";
+
+const Category = () => {
+  return (
+    <div className={styles.gridContainer}>
+      <div className={styles.titleBar}></div>
+      <Header></Header>
+      <Search></Search>
+      <SideBar></SideBar>
+      <ProductGrid></ProductGrid>
+    </div>
+  );
+};
+
+export default Category;
