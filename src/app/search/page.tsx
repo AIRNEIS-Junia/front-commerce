dd34e6b4befc1b68733cd3ed37048fe5bf2fb2c7
AntSch <antoine97410@hotmail.fr>
2024-04-01 13:53:45 +0200

    Improve search page base on category page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ProductGrid from "@/components/Shop/ProductGrid/ProductGrid";
-import Header from "@/components/Shop/Header/Header";
 import SideBar from "@/components/Shop/SideBar/SideBar";
 import SearchResult from "@/components/Shop/SearchResult/SearchResult";
 import styles from "./search.module.css";
 
 const Search = () => {
   return (
-    <div className={styles.gridContainer}>
+    <div className={`${styles.gridContainer} mt-40`}>
       <div className={styles.titleBar}></div>
       <div className={styles.all}>
         <h2>All</h2>
--- src/components/Shop/SearchResult/SearchResult.tsx
+++ src/components/Shop/SearchResult/SearchResult.tsx
@@ -1,3 +1,4 @@
+"use client";
 import styles from "./SearchResult.module.css";
 import React from "react";
 import Image from "next/image";
