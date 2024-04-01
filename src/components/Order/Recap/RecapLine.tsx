9c658b620c5bea1e5052ed98674d0187ce1e1fa2
AntSch <antoine97410@hotmail.fr>
2024-04-01 13:52:54 +0200

    Add orders page

---
+++ src/components/Order/Recap/RecapLine.tsx
@@ -0,0 +1,19 @@
+// RecapLine.js
+import React from "react";
+
+const RecapLine = () => {
+  return (
+    <div className={"[&:not(:last-child)]:border-b border-oyster"}>
+      <div className={"flex justify-between flex-row p-4"}>
+        <p className={"font-bold"}>2022/10/03 - 3912309</p>
+        <p className={"italic"}>IN PROGRESS</p>
+      </div>
+      <div className={"flex justify-between flex-row p-4"}>
+        <p>5 articles</p>
+        <p>1 200€</p>
+      </div>
+    </div>
+  );
+};
+
+export default RecapLine;
