56113424220c8ae03cd6d53801bd25697f9b6aa2
Antsch <antoine97410@hotmail.fr>
2024-04-01 10:36:04 +0200

    Add drawers functions for Filters and SortBy

--- src/components/Shop/SideBar/SideBar.tsx
+++ src/components/Shop/SideBar/SideBar.tsx
@@ -1,36 +1,50 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "./SideBar.module.css";
 
-const SideBar = () => {
+function SideBar() {
+  const [isFiltersOpen, setIsFiltersOpen] = useState(false);
+  const [isSortByOpen, setIsSortByOpen] = useState(false);
+
   return (
     <div>
       <div className={styles.sidebar}>
-        <div className={styles.sidebarTitle}>
+        <div
+          className={styles.sidebarTitle}
+          onClick={() => setIsFiltersOpen(!isFiltersOpen)}
+        >
           <p>FILTERS</p>
         </div>
-        <div className={styles.sidebarCriteria}>
-          <ul>
-            <li>DECORS</li>
-            <li>CERAMICS</li>
-            <li>CHAIRS</li>
-            <li>LAMP</li>
-          </ul>
-        </div>
-        <div className={styles.sidebarTitle}>
+        {isFiltersOpen && (
+          <div className={styles.sidebarCriteria}>
+            <ul>
+              <li>DECORS</li>
+              <li>CERAMICS</li>
+              <li>CHAIRS</li>
+              <li>LAMP</li>
+            </ul>
+          </div>
+        )}
+        <div
+          className={styles.sidebarTitle}
+          onClick={() => setIsSortByOpen(!isSortByOpen)}
+        >
           <p>SORT BY</p>
         </div>
-        <div className={styles.sidebarCriteria}>
-          <ul>
-            <li>PRICE: LOW TO HIGH</li>
-            <li>PRICE: HIGH TO LOW</li>
-            <li>ALPH: A TO Z</li>
-            <li>ALPH: Z TO A</li>
-            <li>BEST SELLING</li>
-          </ul>
-        </div>
+        {isSortByOpen && (
+          <div className={styles.sidebarCriteria}>
+            <ul>
+              <li>PRICE: LOW TO HIGH</li>
+              <li>PRICE: HIGH TO LOW</li>
+              <li>ALPH: A TO Z</li>
+              <li>ALPH: Z TO A</li>
+              <li>BEST SELLING</li>
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
-};
+}
 
 export default SideBar;
